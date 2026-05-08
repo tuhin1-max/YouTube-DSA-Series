7808d1f6c2f5e389e2f2412071b2f5b7bc41adbd
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-08 02:38:21 +0000

    May 8, 2026, 8:38 AM

--- Chapter 1/I_Palindrome.cpp
+++ Chapter 1/I_Palindrome.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
     char s[1005];
     cin >> s;
-    
-    int count = 0;
-    for(int i=0; s[i] != '\0'; i++){
-        count++;
-    }
+    int count = strlen(s);
+    // int count = 0;
+    // for(int i=0; s[i] != '\0'; i++){
+    //     count++;
+    // }
     
     int flag = 1;
     for(int i=0,j=count-1;i<j;i++,j--){
