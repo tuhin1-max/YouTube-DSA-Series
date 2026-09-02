a6d70f7e080458cd650ebbe01f67adf80fd1c162
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-09-03 00:29:25 +0600

    Add function to return dynamic array from function in Chapter 2
    - Implemented a function that allocates a dynamic array of integers.
    - The function takes user input to populate the array.
    - Main function demonstrates the usage of the dynamic array by printing its contents.

--- Chapter 2/dynamic_variable.cpp
+++ Chapter 2/dynamic_variable.cpp
@@ -1,17 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int *p;
 void fun(){
     int *x = new int;
-    *x = 10;
-    p=x;
-    cout << "fun " << *p << endl;
+    *x = 100;
+    p = x;
+    cout << "fun: " << *p << endl;
     return;
-
 }
 int main(){
-    fun();
-    cout << "Main " << *p << endl;
+   fun();
+   cout << "main: " << *p << endl;
     return 0;
 }
